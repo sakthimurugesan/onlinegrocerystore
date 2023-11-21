2a76265e58ffc362d3f20fa92a56fda98015a2c6
SAKTHI M <msakthi150@gmail.com>
2023-11-21 18:53:56 +0530

    commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,77 +1,109 @@
-import React, { useState } from 'react';
-import Navbar from './Navbar'
-import '../styles/Cart.css'
-import Footer from './Footer'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import '../styles/ProductPage.css';
+import productsData from './products.json';
 
-const Cart = () => {
-  const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart')) || []);
-  const [serialNumber, setSerialNumber] = useState(1);
-
-  const deleteProduct = (productId) => {
-    // Display a confirmation dialog
-    const isConfirmed = window.confirm("Are you sure you want to delete this product?");
-  
-    if (isConfirmed) {
-      // If confirmed, update the cart
-      const updatedCart = cartItems.filter((item) => item.id !== productId);
-      setCartItems(updatedCart);
-  
-      // Update local storage
-      localStorage.setItem('cart', JSON.stringify(updatedCart));
-      toast.error("Product deleted")
+const ProductPage = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [selectValue, setSelectValue] = useState(1);
+  const handleInputChange = (e) => {
+    const inputValue = parseInt(e.target.value, 10);
+    if (!isNaN(inputValue)) {
+      setSelectValue(inputValue);
     }
+    // Additional logic if needed based on input value
+    // ...
   };
-  const calculateTotalAmount = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-  
-  return (
-    <div>
-      <Navbar></Navbar>
-      <ul>
+  useEffect(() => {
+    const selectedProduct = productsData.find((item) => item.id == id);
+    setProduct(selectedProduct);
+  }, [id]);
 
-      </ul>
-      <div className="box-1">
-        <div className="table-1">
-          <div className="row-1 header">
-            <div className="cell-1">SNo</div>
-            <div className="cell-1">Product Name</div>
-            <div className="cell-1">Quantity</div>
-            <div className="cell-1">Price</div>
-            <div className="cell-1">Total Price</div>
-            <div className="cell-1"></div>
+  const handleSelectChange = (e) => {
+    setSelectValue(parseInt(e.target.value, 10));
+  };
+
+  const handleAddToCart = () => {
+    if (!product) {
+      return;
+    }
+  
+    // Get existing cart items from local storage
+    const existingCartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  
+    // Check if the product is already in the cart
+    const existingProductIndex = existingCartItems.findIndex((item) => item.id === product.id);
+  
+    if (existingProductIndex !== -1) {
+      // If the product is already in the cart, update the quantity
+      existingCartItems[existingProductIndex].quantity += selectValue;
+    } else {
+      // If the product is not in the cart, add it with the selected quantity
+      existingCartItems.push({ ...product, quantity: selectValue });
+    }
+  
+    // Update the cart in local storage
+    localStorage.setItem('cart', JSON.stringify(existingCartItems));
+    alert('Product added to cart!');
+  };
+
+  if (!product) {
+    return <p>Product not found</p>;
+  }
+
+  const { title, type, price, rating, filename } = product;
+  const handleBlur = (e) => {
+    const inputValue = parseInt(e.target.value, 10);
+    if (!isNaN(inputValue)) {
+      setSelectValue(inputValue);
+    }
+    // Additional logic if needed based on input value
+    // ...
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className='product-main-div'>
+        <div className='product-main-div-1'>
+          <div className='product-img-div'>
+            <img src={'/img/' + filename} alt={title} />
           </div>
-          {cartItems.map((item, index) => (
-            <div className="row-1" key={item.id}>
-              <div className="cell-1" data-title="SNo">{serialNumber + index}</div>
-              <div className="cell-1" data-title="Product Name">{item.title}</div>
-              <div className="cell-1" data-title="Quantity">{item.quantity}</div>
-              <div className="cell-1" data-title="Price">$ {item.price.toFixed(2)}</div>
-              <div className="cell-1" data-title="Total Price">$ {(item.price * item.quantity).toFixed(2)}</div>
-              <div className="cell-1" data-title="Delete">
-                <button className='del-btn' onClick={() => deleteProduct(item.id)}>Delete</button>
-              </div>
+          <div className='product-detail-div'>
+            <h1>{title}</h1>
+            <h6>Category {type}</h6>
+            <h6>Price ${price}</h6>
+            <h6>Rating {rating}/5</h6>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit
+              amet fringilla lorem. Interdum et malesuada fames ac ante ipsum
+              primis in faucibus. Pellentesque quis ullamcorper nibh, eget
+           
+            </p>
+            <div className='quantity-counter' style={{
+              display:"flex"
+            }}>
+              <button style={{width:'1px' , }} onClick={()=>setSelectValue(selectValue-1)}>-</button>
+              <input
+          type='number' min={1} max={100}
+          value={selectValue}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+        />
+            <button style={{width:'1px' , }} onClick={()=>setSelectValue(selectValue+1)}>+</button>
             </div>
-          ))}
-            <div className='row-1'>
-            <div className="cell-1"></div>
-            <div className="cell-1"></div>
-            <div className="cell-1"></div>
-            <div className="cell-1">Total Amount</div>
-            <div className="cell-1" style={{
-              color:"black"
-            }}>${calculateTotalAmount().toFixed(2)}</div>
-            <div className="cell-1"><a href='/'><button className='chk-btn'>CheckOut</button></a></div>
-            </div>
+            <button type="button" onClick={handleAddToCart}>
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
-      <Footer></Footer>
-      <ToastContainer></ToastContainer>
-      
-    </div>
+      <Footer />
+    </>
   );
 };
 
-export default Cart;
+export default ProductPage;

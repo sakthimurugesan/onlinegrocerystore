7e2f32092c941a4ebca2ba95edc6f783c02890c7
SAKTHI M <msakthi150@gmail.com>
2023-11-25 21:03:00 +0530

    commit

--- src/components/Addpage.js
+++ src/components/Addpage.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useParams ,useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const AddProduct = ({ fetchData }) => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     title: '',
     description: '',
     rating: 0,
     price: 0,
-    "type": "fruit","height": 600,
-    "width": 401, "trend": ""
-    ,id:500,
-    "filename": "48.jpg",
+    type: 'fruit', // Default to 'fruit'
+    height: 600,
+    width: 401,
+    trend: '',
+    id: 500,
+    filename: '48.jpg',
   });
 
   const handleChange = (e) => {
@@ -21,48 +25,88 @@ const AddProduct = ({ fetchData }) => {
     });
   };
 
-  const handleImageChange = (e) => {
-    setFormData({
-      ...formData,
-      
-    });
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // Assuming your API expects the image to be uploaded separately
-    
-
       const productData = {
         title: formData.title,
         description: formData.description,
         rating: formData.rating,
-        price: formData.price, "type": "fruit","height": 600,
-        "width": 401, "trend": ""
-        ,
-        "filename": "48.jpg",
-        
+        price: formData.price,
+        type: formData.type,
+        height: 600,
+        width: 401,
+        trend: '',
+        filename: '48.jpg',
       };
 
       await axios.post('http://localhost:3001/books', productData);
 
       alert('Product added successfully');
-      navigate('/dashboard')
+      navigate('/dashboard');
       // Fetch updated data
       fetchData();
     } catch (error) {
-      console.error('Error adding product:');
+      console.error('Error adding product:', error);
     }
   };
 
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  };
+
+  const formStyle = {
+    width: '300px',
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  };
+
+  const labelStyle = {
+    display: 'block',
+    marginBottom: '8px',
+  };
+
+  const inputStyle = {
+    width: '100%',
+    padding: '8px',
+    marginBottom: '16px',
+  };
+
+  const selectStyle = {
+    width: '100%',
+    padding: '8px',
+    marginBottom: '16px',
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#4caf50',
+    color: 'white',
+    padding: '10px 15px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  };
+
+  const buttonHoverStyle = {
+    backgroundColor: '#45a049',
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <h1>Add New Product</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={formStyle}>
         <div>
-          <label htmlFor="title">Title:</label>
+          <label htmlFor="title" style={labelStyle}>
+            Title:
+          </label>
           <input
             type="text"
             id="title"
@@ -70,20 +114,26 @@ const AddProduct = ({ fetchData }) => {
             value={formData.title}
             onChange={handleChange}
             required
+            style={inputStyle}
           />
         </div>
         <div>
-          <label htmlFor="description">Description:</label>
+          <label htmlFor="description" style={labelStyle}>
+            Description:
+          </label>
           <textarea
             id="description"
             name="description"
             value={formData.description}
             onChange={handleChange}
             required
+            style={inputStyle}
           />
         </div>
         <div>
-          <label htmlFor="rating">Rating:</label>
+          <label htmlFor="rating" style={labelStyle}>
+            Rating:
+          </label>
           <input
             type="number"
             id="rating"
@@ -91,10 +141,13 @@ const AddProduct = ({ fetchData }) => {
             value={formData.rating}
             onChange={handleChange}
             required
+            style={inputStyle}
           />
         </div>
         <div>
-          <label htmlFor="price">Price:</label>
+          <label htmlFor="price" style={labelStyle}>
+            Price:
+          </label>
           <input
             type="number"
             id="price"
@@ -102,13 +155,37 @@ const AddProduct = ({ fetchData }) => {
             value={formData.price}
             onChange={handleChange}
             required
+            style={inputStyle}
           />
         </div>
         <div>
-          
+          <label htmlFor="type" style={labelStyle}>
+            Category:
+          </label>
+          <select
+            id="type"
+            name="type"
+            value={formData.type}
+            onChange={handleChange}
+            style={selectStyle}
+          >
+            <option value="dairy">Dairy</option>
+            <option value="fruit">Fruit</option>
+            <option value="vegetable">Vegetable</option>
+            <option value="bakery">Bakery</option>
+            <option value="vegan">Vegan</option>
+            <option value="meat">Meat</option>
+          </select>
         </div>
         <div>
-          <button type="submit">Add Product</button>
+          <button
+            type="submit"
+            style={buttonStyle}
+            
+            
+          >
+            Add Product
+          </button>
         </div>
       </form>
     </div>
--- src/components/UpdatePage.js
+++ src/components/UpdatePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams ,useNavigate} from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const UpdatePage = () => {
   const { id } = useParams();
@@ -9,7 +9,8 @@ const UpdatePage = () => {
     price: 0,
     rating: 0,
   });
-const navigate=useNavigate()
+  const navigate = useNavigate();
+
   useEffect(() => {
     fetchData();
   }, [id]);
@@ -35,49 +36,105 @@ const navigate=useNavigate()
     try {
       await axios.put(`http://localhost:3001/books/${id}`, formData);
       console.log('Item updated successfully');
-      navigate('/')
+      navigate('/dashboard');
       // Redirect or perform other actions after successful update
     } catch (error) {
       console.error('Error updating item:', error);
     }
   };
 
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  };
+
+  const formStyle = {
+    width: '300px',
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  };
+
+  const labelStyle = {
+    display: 'block',
+    marginBottom: '8px',
+  };
+
+  const inputStyle = {
+    width: '100%',
+    padding: '8px',
+    marginBottom: '16px',
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#4caf50',
+    color: 'white',
+    padding: '10px 15px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  };
+
+  const buttonHoverStyle = {
+    backgroundColor: '#45a049',
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <h1>Update Item</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={formStyle}>
         <div>
-          <label htmlFor="title">Title:</label>
+          <label htmlFor="title" style={labelStyle}>
+            Title:
+          </label>
           <input
             type="text"
             id="title"
             name="title"
             value={formData.title}
             onChange={handleChange}
+            style={inputStyle}
           />
         </div>
         <div>
-          <label htmlFor="price">Price:</label>
+          <label htmlFor="price" style={labelStyle}>
+            Price:
+          </label>
           <input
             type="number"
             id="price"
             name="price"
             value={formData.price}
             onChange={handleChange}
+            style={inputStyle}
           />
         </div>
         <div>
-          <label htmlFor="rating">Rating:</label>
+          <label htmlFor="rating" style={labelStyle}>
+            Rating:
+          </label>
           <input
             type="number"
             id="rating"
             name="rating"
             value={formData.rating}
             onChange={handleChange}
+            style={inputStyle}
           />
         </div>
         <div>
-          <button type="submit">Update Item</button>
+          <button
+            type="submit"
+            style={buttonStyle}
+            
+          >
+            Update Item
+          </button>
         </div>
       </form>
     </div>
